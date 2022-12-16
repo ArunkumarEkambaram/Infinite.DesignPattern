52ca83378cc0681a7ff346a1587519d3a7c9861b
ArunKumar <arunkumar.e@outlook.com>
2022-12-16 12:56:47 +0530

    Created new class in Factory

---
+++ Infinite.DesignPattern.Factory/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infinite.DesignPattern.Factory
+{
+    class TestClass
+    {
+        public void Add()
+        {
+            Console.WriteLine("Add Method");
+        }
+    }
+}
